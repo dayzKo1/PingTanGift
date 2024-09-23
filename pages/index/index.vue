4d19fbb342d2088298621a301b05373d9af3e9c1
linhonghao <845003566@qq.com>
2024-09-23 13:59:13 +0800

    init

---
+++ pages/index/index.vue
@@ -0,0 +1,68 @@
+<template>
+	<view class="content">
+		<image class="logo" src="/static/logo.png"></image>
+		<view class="text-area">
+			<text class="title">{{title}}</text>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+			uniCloud.callFunction({
+				name: 'pubAdmin',
+				data: {
+					url: 'sysAdmin/login',
+					data: {
+						userName: "123",
+						password: "456"
+					},
+					userInfo: {
+						token: "123456"
+					}
+				}
+			}).then((res) => {
+				console.log(res.result) 
+			}).catch((err) => {
+				console.error(err)
+			})
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
